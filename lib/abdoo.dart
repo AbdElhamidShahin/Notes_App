ab4a165cce3089d00d1eca29b02f97a5febae713
AbdElhamid Shahin <abdelhamid.shahin0@gmail.com>
2024-05-17 20:51:38 +0300

    abdo

---
+++ lib/abdoo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class abdo extends StatelessWidget {
+  const abdo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
